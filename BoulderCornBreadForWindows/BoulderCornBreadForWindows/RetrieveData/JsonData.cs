56c3d45cef21a884982236a9d74c152a0fd9548d
Kelly Martinez <anthem001@gmail.com>
2013-04-28 01:55:18 -0600

    added jsondata class

---
+++ BoulderCornBreadForWindows/BoulderCornBreadForWindows/RetrieveData/JsonData.cs
@@ -0,0 +1,24 @@
+﻿namespace BoulderCornBreadForWindows.RetrieveData
+{
+    class JsonData
+    {
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string SchoolName { get; set; }
+        public string Fellow { get; set; }
+        public string FellowWebsite { get; set; }
+        public string FellowImage { get; set; }
+        public string FellowImageHeight { get; set; }
+        public string FellowImageWidth { get; set; }
+        public string RoleDescription { get; set; }
+        public int Year { get; set; }
+        public string GradesServed { get; set; }
+        public string NumOfStudents { get; set; }
+        public string EligibleLunch { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Website { get; set; }
+        public string Address { get; set; }
+
+    }
+}
