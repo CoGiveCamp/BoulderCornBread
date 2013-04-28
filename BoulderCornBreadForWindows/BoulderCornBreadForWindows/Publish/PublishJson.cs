a5f326c43347008fce98df120a26651bbb5e58cd
Gabriel <extofer@gmail.com>
2013-04-28 02:29:47 -0600

    fuck this

---
+++ BoulderCornBreadForWindows/BoulderCornBreadForWindows/Publish/PublishJson.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BoulderCornBreadForWindows.Publish
+{
+    class PublishJson
+    {
+
+        public Boolean PublishJsontoFtp(Object json, string filename, string filepath)
+        {
+            // fuck this shit
+            string jsonToWrite =  json.ToString();
+            WriteToFile();
+            return false;
+        }
+
+        void WriteToFile()
+        {
+
+            using (StreamWriter writer = new StreamWriter("debug.txt", true))
+            {
+                writer.WriteLine("whatever you text is");
+            }
+
+        }
+
+    }
+
+}
