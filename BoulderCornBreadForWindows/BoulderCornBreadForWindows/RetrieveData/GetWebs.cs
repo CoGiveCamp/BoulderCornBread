8342e51602b13d4942c6e1d4b0ebb78074e98058
Kelly Martinez <anthem001@gmail.com>
2013-04-28 01:50:45 -0600

    added jsondata class and methods on getwebs. fixed designer issues

--- BoulderCornBreadForWindows/BoulderCornBreadForWindows/RetrieveData/GetWebs.cs
+++ BoulderCornBreadForWindows/BoulderCornBreadForWindows/RetrieveData/GetWebs.cs
@@ -1,21 +1,31 @@
-﻿using System.Net;
+﻿using System.Collections.Generic;
+using System.Net;
 
 namespace BoulderCornBreadForWindows.RetrieveData
 {
-    class GetWebs
+    internal class GetWebs
     {
 
         // retrieve JSON from some url
-        public string GetJsonFromUrl(string urlPath)
+        private string GetJsonFromUrl(string urlPath)
         {
             var json = new WebClient().DownloadString(urlPath);
             return json;
         }
 
         // parse out JSON to DataGrid
-        public void SetDataGridViewJson(string input)
+        public List<JsonData> ParseJson(string url)
         {
-            
+
+            url = "http://getsmartschools.org/currentschools.txt";
+            var json = GetJsonFromUrl(url);
+            var dataList = new List<JsonData>
+                {
+                    new JsonData()
+                };
+
+            return dataList;
         }
     }
+
 }
