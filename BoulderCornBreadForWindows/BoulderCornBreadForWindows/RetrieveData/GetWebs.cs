4936b714a734f2a0b46fabff53a6a672a90e8d6f
Kelly Martinez <anthem001@gmail.com>
2013-04-28 01:25:03 -0600

    added retrive data folder and getwebs class

---
+++ BoulderCornBreadForWindows/BoulderCornBreadForWindows/RetrieveData/GetWebs.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+
+namespace BoulderCornBreadForWindows.RetrieveData
+{
+    class GetWebs
+    {
+
+        // retrieve JSON from some url
+        public string GetJsonFromUrl(string urlPath)
+        {
+            var json = new WebClient().DownloadString(urlPath);
+            return json;
+        }
+
+        // parse out JSON to DataGrid
+        public void SetDataGridViewJson(string input)
+        {
+            
+        }
+    }
+}
