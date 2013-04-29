521c36a67ac12577a59045ce9f90c8fce46d9f97
Jason <jkassay@gmail.com>
2013-04-29 09:15:19 -0600

    changed form to load data

--- BoulderCornBreadForWindows/BoulderCornBreadForWindows/Form1.Designer.cs
+++ BoulderCornBreadForWindows/BoulderCornBreadForWindows/Form1.Designer.cs
@@ -28,38 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.gvJson = new System.Windows.Forms.DataGridView();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gvJson)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // gvJson
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(658, 238);
-            this.dataGridView1.TabIndex = 0;
+            this.gvJson.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gvJson.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvJson.Location = new System.Drawing.Point(12, 12);
+            this.gvJson.Name = "gvJson";
+            this.gvJson.Size = new System.Drawing.Size(990, 238);
+            this.gvJson.TabIndex = 0;
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Location = new System.Drawing.Point(13, 279);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(75, 23);
+            this.btnAddNew.TabIndex = 1;
+            this.btnAddNew.Text = "Add New";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            this.btnAddNew.Click += new System.EventHandler(this.btnAddNew_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(926, 278);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-
-            this.ClientSize = new System.Drawing.Size(682, 262);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(1014, 327);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnAddNew);
+            this.Controls.Add(this.gvJson);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvJson)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView gvJson;
+        private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.Button btnSave;
     }
 }
 
--- BoulderCornBreadForWindows/BoulderCornBreadForWindows/Form1.cs
+++ BoulderCornBreadForWindows/BoulderCornBreadForWindows/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using BoulderCornBreadForWindows.RetrieveData;
 
 namespace BoulderCornBreadForWindows
 {
@@ -14,6 +15,26 @@ namespace BoulderCornBreadForWindows
         public Form1()
         {
             InitializeComponent();
+
+            BindGrid();
         }
-    }
-}
+
+        private void BindGrid()
+        {
+            GetWebs gb = new GetWebs();
+
+            this.gvJson.DataSource = gb.ParseJson("http://getsmartschools.org/currentschools.txt");
+        }
+
+        private void btnAddNew_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You clicked the Save button. You get a swift kick to the groin.");
+        }
+
+    }   //end class Form1
+}   //end namespace
