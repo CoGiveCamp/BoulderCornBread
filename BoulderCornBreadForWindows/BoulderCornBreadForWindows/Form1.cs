a20e35fdee76ff6300431cde7f79c7de87983d7e
Kelly Martinez <anthem001@gmail.com>
2013-05-04 20:02:00 -0600

    added json saving serializer

--- BoulderCornBreadForWindows/BoulderCornBreadForWindows/Form1.cs
+++ BoulderCornBreadForWindows/BoulderCornBreadForWindows/Form1.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using BoulderCornBreadForWindows.RetrieveData;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace BoulderCornBreadForWindows
 {
@@ -27,12 +30,28 @@ namespace BoulderCornBreadForWindows
 
         private void btnAddNew_Click(object sender, EventArgs e)
         {
-           
+
+        }
+
+        private List<dynamic> ConvertGridViewToCollection(DataGridView dg)
+        {
+            var data = (List<dynamic>)dg.DataSource;
+            return data;
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("You clicked the Save button. You get a swift kick to the groin.");
+            var serializer = new JsonSerializer();
+            serializer.Converters.Add(new JavaScriptDateTimeConverter());
+
+            using (var sw = new StreamWriter(@"data.json"))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                var data = ConvertGridViewToCollection(gvJson);
+                serializer.Serialize(writer, data);
+
+            }
+
         }
 
     }   //end class Form1
